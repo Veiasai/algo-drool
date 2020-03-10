e8a21cce89eee420201cb2c2af31aae1057918d2
Veiasai <o206@qq.com>
2020-03-10 18:19:14 +0800

    webfigure plot

--- src/main/java/algo/drool/service/WeibullService.java
+++ src/main/java/algo/drool/service/WeibullService.java
@@ -1,24 +1,39 @@
 package algo.drool.service;
 
-import WeibullJA.Class1;
+import com.mathworks.toolbox.javabuilder.MWArray;
 import com.mathworks.toolbox.javabuilder.MWException;
+import com.mathworks.toolbox.javabuilder.MWJavaObjectRef;
 import com.mathworks.toolbox.javabuilder.MWNumericArray;
+import com.mathworks.toolbox.javabuilder.webfigures.WebFigure;
+import getfigure.Getfigure;
+import getparameter.Getparameter;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WeibullService {
 
-    private Class1 class1;
+    private Getfigure getfigure;
+    private Getparameter getparameter;
 
     public WeibullService() throws MWException {
-        this.class1 = new Class1();
+        this.getfigure = new Getfigure();
+        this.getparameter = new Getparameter();
     }
 
     public Object evaluate() throws MWException {
         Object[] result = {null};
         Object[] input = {3029D, 3124D, 3248D, 3345D, 3570D, 3755D, 3974D, 3993D, 4254D, 4357D};
-        class1.WeibullJA(result, input);
+        getparameter.getparameter(result, input);
         MWNumericArray numericArray = (MWNumericArray)result[0];
-        return numericArray.toDoubleArray();
+        return plot(numericArray);
+//        return numericArray.toDoubleArray();
+    }
+
+    public Object plot(MWNumericArray array) throws MWException {
+        Object[] result = {null};
+        Object[] input = {array};
+        getfigure.getfigure(result, input);
+        WebFigure webFigure = (WebFigure) ((MWJavaObjectRef)result[0]).get();
+        return webFigure;
     }
 }
---
+++ src/test/java/algo/drool/service/WeibullServiceTest.java
@@ -0,0 +1,34 @@
+package algo.drool.service;
+
+import com.mathworks.toolbox.javabuilder.MWException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeibullServiceTest {
+
+    private WeibullService weibullService;
+
+    @BeforeAll
+    void init() {
+        try {
+            weibullService = new WeibullService();
+        } catch (MWException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void evaluate() {
+        try {
+            System.out.println(weibullService.evaluate());
+        } catch (MWException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void plot() {
+    }
+}
