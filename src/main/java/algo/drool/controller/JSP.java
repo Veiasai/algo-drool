b6b875c2a1ea1fe2cc49a4b8f7bd73bdd3a97578
Veiasai <o206@qq.com>
2020-03-11 08:50:50 +0800

    session attribute

--- src/main/java/algo/drool/controller/JSP.java
+++ src/main/java/algo/drool/controller/JSP.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes(value = {"MyFigure"})
 public class JSP {
     @Autowired
     private WeibullService weibullService;
